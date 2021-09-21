569f1c21f1a1e1fcf2aab964c275e29fb5220472
Andrey <phobos05@bk.ru>
2021-09-21 21:17:09 +0700

    03_Variables_types исправил равенство

--- 03_Variables_types/src/Main.kt
+++ 03_Variables_types/src/Main.kt
@@ -3,14 +3,11 @@ fun main() {
 
     val firstName = "Andrey"
     val lastName = "Adamov"
-    var height: Double
-    val weight: Float
-    var isChild = false
 
-    height = 178.0
-    weight = 75f
+    var height = 178.0
+    val weight = 75f
 
-    isChild = height <= 150.0 && weight <= 40f
+    var isChild:Boolean = height < 150.0 && weight < 40f
     var info: String = """
             | First Name: $firstName, 
             | Last Name: $lastName, 
@@ -23,7 +20,7 @@ fun main() {
 
     height = 190.0
 
-    isChild = height <= 150.0 && weight <= 40f
+    isChild = height < 150.0 && weight < 40f
     info = """
             | First Name: $firstName, 
             | Last Name: $lastName, 
