533c66ce8e9c3e8d1366ffd4f33301944e842e0f
Andrey <phobos05@bk.ru>
2021-09-25 15:37:03 +0700

    home work #4

--- 03_Variables_types/src/04.kt
+++ 03_Variables_types/src/04.kt
@@ -6,15 +6,51 @@ fun main() {
     var sourceString1 = sourceString.substring(0, m)
     var sourceString2 = sourceString.substring(m)
 
+    val x: (Char) -> Char = { char -> char + 1 }
+    val y: (Char) -> Char = { char -> char - 3 }
+    val z: (Char) -> Char = { char -> char - 4 }
 
     println(replaceString1(sourceString1) + replaceString2(sourceString2))
+
+
+  /*  val firstList = listOf(1, 4, 10)
+    val a = { n: Int, m: Int -> println(n + m) }
+    val mult: (Int) -> Int = { it * 2 }
+    a(10, 20)
+    a(20, 50)
+    val multList = mathWithList(firstList, mult)
+
+    println(multList)*/
 
 }
 
 
+fun shift(stroka:String, shifting: (Char) -> Char):String{
+
+    val shiftedString = stroka.map (shifting).joinToString("")
+    return  shiftedString
+}
+
+/*
+fun mathWithList(yourList: List<Int>, math: (Int) -> Int): List<Int> {
+    val newList = mutableListOf<Int>()
+    for (i in yourList) {
+        newList.add(math(i))
+    }
+    return newList
+}
+*/
+
+/*fun shift(str: String, sifting: (Char) -> Char): String {
+
+    //val shiftedString = str.map { char -> char + 1 }.joinToString("")
+    return shiftedString
+}*/
+
 fun replaceString1(str: String): String {
 
-    val shiftedString = str.map { char -> char + 1 }.joinToString("")
+    //val shiftedString = str.map { char -> char + 1 }.joinToString("")
+    val shiftedString = str.map { char -> char+1}.joinToString("")
     //println("сдвиг символов          $shiftedString")
 
     val replaceString1 = shiftedString.replace('4', 'u')
@@ -31,16 +67,13 @@ fun replaceString1(str: String): String {
 
 }
 
-fun shift(str: String, shiftedSymbol: Int):String {
-    str.map { char -> char - shiftedSymbol }.joinToString("")
-    return str
-}
-
-
 fun replaceString2(str: String): String {
+
+
     val reversString = str.reversed()
     //println("разворот строки         $reversString")
 
+    // val shiftedString = reversString.map { char -> char - 4 }.joinToString("")
     val shiftedString = reversString.map { char -> char - 4 }.joinToString("")
     // println("сдвиг символов          $shiftedString")
 
@@ -53,13 +86,6 @@ fun replaceString2(str: String): String {
 }
 
 
-
-
-
-fun convert(x: Double, converter: (Double) -> Double): Double {
-    // ваш код
-    return x
-}
 
 
 
