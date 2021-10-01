fa4e9a6a6cb60e8ee862f36f6ce64c12f7a57002
Andrey <phobos05@bk.ru>
2021-10-01 23:17:52 +0700

    fibo2

--- 05_ConditionalExpressions_Loops/Homework2/src/Fibonacci.kt
+++ 05_ConditionalExpressions_Loops/Homework2/src/Fibonacci.kt
@@ -1,17 +1,39 @@
 fun main() {
-    var n:Int
+    var n: Int
 
     do {
         println("input number >0")
-         n = readLine()?.toIntOrNull()?:return
-    }while (n<=0)
-   println(fibo(n))
-   println(Int.MAX_VALUE)
+        n = readLine()?.toIntOrNull() ?: return
+    } while (n <= 0)
+    println(fibo(n))
+    var l = Long.MAX_VALUE
+println("Lon $l")
 }
 
-fun fibo(number: Int): Int {
+/*fun fibo(number: Int): Int {
+    return if (number <= 1) {
+        0;
+    } else if (number == 2) {
+        1;
+    } else  {
+        fibo(number - 1) + fibo(number - 2);
+    }
 
 
-    return if (number <= 1) 0 else if (number == 2) 1 else fibo(number - 1) + fibo(number - 2)
 
+
+}*/
+
+fun fibo(number: Int):Long {
+    //val n = 10
+    val arr = LongArray(number)
+    arr[0] = 0
+    arr[1] = 1
+    for (i in 2 until arr.size) {
+        arr[i] = arr[i - 1] + arr[i - 2]
+        System.out.println(arr[i])
+    }
+    return arr.maxOf { it }.toLong()
 }
+
+
