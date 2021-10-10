ab342de2fed10d44e6fac96a4201d64c176c4507
Andrey <phobos05@bk.ru>
2021-10-10 22:55:12 +0700

    9

--- 06_Collections/src/Collect.kt
+++ 06_Collections/src/Collect.kt
@@ -1,5 +1,3 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -8,21 +6,34 @@ fun main() {
     println("Enter array length: ")
     val size = input.nextInt() // Читаем с клавиатуры размер массива и записываем в size
 
-    inputFun(size)
-    funMutableMap()
+
+
+    //var myArray=data
+
+    getPhones(size)
+   // funMutableMap()
 }
 
-fun inputFun(size: Int): MutableList<String> {
-    val input = Scanner(System.`in`)
+
+
+
+fun getPhones(size:Int): MutableList<String> {
+   /* //val input = Scanner(System.`in`)
+   // val input = readLine()*/
+
     val data: MutableList<String> = ArrayList() // Создаём массив int размером в size
-    println("Ведите количчество элементов массива:")
-    for (i in 0 until size) {//Пройдёмся по всему массиву, заполняя его
+    println("Ведите элементы массива:")
+    /*for (i in 0 until size) {//Пройдёмся по всему массиву, заполняя его
         data.add(input.next()) // Заполняем массив элементами, введёнными с клавиатуры
+    }*/
+
+    repeat(size){
+        data.add(readLine().toString())
     }
     print("Введите элементы массива:")
     println(data)
 
-    val filterData = data.filter { it.contains("+7") }
+    val filterData = data.filter { it.startsWith("+7") }
     println("""Номера телефонов начинающиеся с "+7" $filterData""")
 
     val toSetData = data.toSet()
@@ -31,28 +42,40 @@ fun inputFun(size: Int): MutableList<String> {
     val sumData = data.sumOf { it.length }
     println("Сумма длин всех номеров телефонов $sumData")
 
-    return filterData as MutableList<String>
+   // return filterData as MutableList<String>
+    return data
 
 }
 
 
-fun funMutableMap() {
+fun getNames(listData: MutableList<String>) {
+
+
 
     println("Ведите номер телефона и имя абонента")
 
-    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val reader = readLine()
+    var map: Map<String?, String?>
     val map: MutableMap<Any, Any> = HashMap()
     while (true) {
-        val id = reader.readLine()
-        if (id == "") break
-        val name = reader.readLine()
+        val id = data
+        //if (id == "") break
+        val name = reader.toString()
         if (name == "") break
         map[name] = id
+    }
+
+    for(i in 0 .. listData.size){
+
+         map = listData.associate {listData[i] to reader }
     }
 
     for ((k, v) in map) {
         println("Абонент: $k. Номер телефона: $v")
     }
+
+
+
 
     val sortByNumber = map.toList().sortedBy { (_, value)-> value as Comparable<Any> }.toMap()
     println(sortByNumber)
