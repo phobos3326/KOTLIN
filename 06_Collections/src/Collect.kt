0bd66333eee218c51354f8d1ca2bbfc02dff99d3
Andrey <phobos05@bk.ru>
2021-10-12 22:06:17 +0700

    9

--- 06_Collections/src/Collect.kt
+++ 06_Collections/src/Collect.kt
@@ -1,75 +1,77 @@
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 fun main() {
     val input = Scanner(System.`in`) // Объявляем Scanner
     println("Enter array length: ")
     val size = input.nextInt() // Читаем с клавиатуры размер массива и записываем в size
-
-
-
     //var myArray=data
 
     getPhones(size)
-   // funMutableMap()
+    var a= getPhones(size)
+    getNames(a)
 }
 
-
-
-
-fun getPhones(size:Int): MutableList<String> {
+fun getPhones(size:Int): List<String> {
    /* //val input = Scanner(System.`in`)
    // val input = readLine()*/
 
-    val data: MutableList<String> = ArrayList() // Создаём массив int размером в size
+    val listOfPhoneNumbers: MutableList<String> = ArrayList() // Создаём массив int размером в size
     println("Ведите элементы массива:")
     /*for (i in 0 until size) {//Пройдёмся по всему массиву, заполняя его
         data.add(input.next()) // Заполняем массив элементами, введёнными с клавиатуры
     }*/
 
     repeat(size){
-        data.add(readLine().toString())
+        listOfPhoneNumbers.add(readLine().toString())
     }
     print("Введите элементы массива:")
-    println(data)
+    println(listOfPhoneNumbers)
 
-    val filterData = data.filter { it.startsWith("+7") }
+    val filterData = listOfPhoneNumbers.filter { it.startsWith("+7") }
     println("""Номера телефонов начинающиеся с "+7" $filterData""")
 
-    val toSetData = data.toSet()
+    val toSetData = listOfPhoneNumbers.toSet()
     println("Уникальные элементы массива $toSetData")
 
-    val sumData = data.sumOf { it.length }
+    val sumData = listOfPhoneNumbers.sumOf { it.length }
     println("Сумма длин всех номеров телефонов $sumData")
 
    // return filterData as MutableList<String>
-    return data
+    return listOfPhoneNumbers
 
 }
 
 
-fun getNames(listData: MutableList<String>) {
+fun getNames(phoneNumber: List<String>) {
 
 
 
     println("Ведите номер телефона и имя абонента")
 
     val reader = readLine()
-    var map: Map<String?, String?>
-    val map: MutableMap<Any, Any> = HashMap()
-    while (true) {
+    var map: Map<String?, String?> = HashMap()
+    //val map: MutableMap<Any, Any> = HashMap()
+    /*while (true) {
         val id = data
         //if (id == "") break
         val name = reader.toString()
         if (name == "") break
         map[name] = id
+    }*/
+
+    for (i in 0 .. phoneNumber.size){
+
+        map = phoneNumber.associateWith { it -> reader }
+
     }
 
-    for(i in 0 .. listData.size){
+    /*for(i in 0 .. listData.size){
 
          map = listData.associate {listData[i] to reader }
     }
-
+*/
     for ((k, v) in map) {
         println("Абонент: $k. Номер телефона: $v")
     }
