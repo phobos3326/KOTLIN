3d6622cbf679aefe30afe6436e78263189f11829
Andrey <phobos05@bk.ru>
2021-10-26 14:30:57 +0700

    work 7 сделал звук + вкл выкл

--- 07_ClassesAndObjects/src/TV.kt
+++ 07_ClassesAndObjects/src/TV.kt
@@ -1,43 +1,42 @@
 class TV(val brand: String, val diagonal: Int, var isOn: Boolean) {
 
-
-    override fun toString() = """$brand, диагональ $diagonal",  ${chekTurnOn()} """
-
-    private fun chekTurnOn() {
-        if (!isOn) {
-            print("выкл")
+    private fun chekTurnOn(): String {
+        val a: String = if (!isOn) {
+            "выключен"
         } else {
-            print("вкл")
+            "включен"
+        }
+        return a
+    }
+
+    var volume = 0
+
+    fun getVolumeUp() {
+        turnOn()
+        for (i in volume..VOLUME) {
+            volume++
+            println("$brand, ${chekTurnOn()}, тест громкости ${volume - 1}")
         }
     }
 
-    fun getVolumeUp(vol: Int): Int {
-        //var volUp = 1
-        return vol + 1
+    fun getVolumeDown() {
+        for (i in volume downTo 1) {
+            volume--
+            println("$brand, ${chekTurnOn()}, тест громкости $volume")
+        }
     }
 
-    fun getVolumeDown(volDown: Int): Int {
-        return volDown - 1
+    private fun turnOn() {
+        if (!isOn) {
+            isOn = true
+        }
     }
 
     companion object {
-        const val VOLUME = 50
+        const val VOLUME = 10
     }
 
-    fun turnOn(): Boolean {
-        if (!isOn) {
-            //isOn = true
-            println("выкл")
-            isOn = true
-        } else {
-            //isOn=true
-            println("вкл")
-        }
-
-
-        return true
-    }
-
+    override fun toString() = """$brand, диагональ $diagonal",  ${chekTurnOn()} """
 
 }
 
--- 07_ClassesAndObjects/src/main.kt
+++ 07_ClassesAndObjects/src/main.kt
@@ -1,47 +1,25 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.*
-import kotlin.random.Random
 
 fun main() {
-    val tvSony = TVClass(brand = "SONY", diagonal = 40, isOn = false)
-    val tvSamsung = TVClass(brand = "Samsung", diagonal = 38, isOn = true)
+    val tvSony = TV(brand = "SONY", diagonal = 40, isOn = false)
+    val tvSamsung = TV(brand = "Samsung", diagonal = 38, isOn = true)
 
-    val map = mutableListOf(tvSony, tvSamsung)
-    println(map.toString())
-    map.forEachIndexed { index, element ->
+    val listOfTV = mutableListOf(tvSony, tvSamsung)
+
+    val selectedTV = selectTv(listOfTV)
+
+    selectedTV.getVolumeUp()
+    println("------------")
+    selectedTV.getVolumeDown()
+
+}
+
+fun selectTv(arrayTv: MutableList<TV>): TV {
+
+    arrayTv.forEachIndexed { index, element ->
         println("${index + 1} : $element")
     }
-    val a = selectTv(map)
 
-    var volValue = TVClass.VOLUME
-
-    var randomVolume = (0..volValue).random()
-    println(randomVolume)
-
-    
-    for (i in 0..randomVolume) {
-        println(" $a громкость ++ ${a.getVolumeUp(i - 1)} ")
-    }
-
-    println("------------")
-
-    for (i in randomVolume downTo 0) {
-        println(" $a громкость -- ${a.getVolumeDown(i + 1)} ")
-    }
-
-
-    /*val reader = BufferedReader(InputStreamReader(System.`in`))
-    val id = reader.readLine()*/
-
-
-}
-
-fun info(tv: TVClass) {
-    println("${tv.brand}, ${tv.diagonal}, ${TVClass.VOLUME}, ${tv.isOn}")
-}
-
-fun selectTv(arrayTv: MutableList<TVClass>): TVClass {
     val input = Scanner(System.`in`)
     println("выберите телевизор из списка")
     val item = input.nextInt()
