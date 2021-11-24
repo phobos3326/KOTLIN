4b9fd79c43f6fc190d7525064a9a4a3f4bc72f26
Andrey <phobos05@bk.ru>
2021-11-24 22:14:19 +0700

    first commit

---
+++ 09_OOP2/src/main/kotlin/BankCard.kt
@@ -0,0 +1,14 @@
+abstract class BankCard() {
+    val balance: Int? = null
+
+    fun balanceUp() {
+    }
+
+    fun pay() {
+    }
+
+    fun getBalance() {
+    println("баланс $balance")
+    }
+
+}
