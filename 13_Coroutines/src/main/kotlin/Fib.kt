1be33c29f0d96bc7b02caee1d11c0a19460eb0ff
Andrey <phobos05@bk.ru>
2022-03-23 09:11:22 +0700

    commit  23.03.22

--- 13_Coroutines/src/main/kotlin/Fib.kt
+++ 13_Coroutines/src/main/kotlin/Fib.kt
@@ -7,43 +7,31 @@ import kotlin.coroutines.CoroutineContext
 
 object Fib {
     val start = System.currentTimeMillis()
-    suspend fun takeIter(n: Int):BigInteger{
-        //currentCoroutineContext().isActive
+    suspend fun takeIter(n: Int): BigInteger {
         yield()
-         var n1 = BigInteger("0")
-         var n2 = BigInteger("1")
+        var n1 = BigInteger("0")
+        var n2 = BigInteger("1")
 
-            if (n < 2) {
-                n.toBigInteger()
-            } else {
-                n1
-                n2
-                var i = 1
-                repeat(n) {
-                    //yield()
-                    i++
-                    val sum = n1 + n2
-                    n1 = n2
-                    n2 = sum
-                }
+        if (n < 2) {
+            n.toBigInteger()
+        } else {
+            n1
+            n2
+            var i = 1
+            repeat(n) {
+                //yield()
+                i++
+                val sum = n1 + n2
+                n1 = n2
+                n2 = sum
             }
-
-            //return@withTimeout n1
+        }
         return n1
-
-//println(res)
-        //checkException()
-
     }
 
     class EcxeptionNumber(message: String) : Exception(message)
-
     private fun exceptionFib() {
-
-
         throw EcxeptionNumber("вычисление слишком долгое")
-
-
     }
 
     private fun checkException() {
@@ -57,32 +45,6 @@ object Fib {
 }
 
 
-object fib2 {
-
-    var a = 0
-    var b = 1
 
 
-    val cache: MutableMap<Int, BigInteger> = mutableMapOf(0 to a.toBigInteger(), 1 to b.toBigInteger())
-    fun take(n: Int): BigInteger = if (n < 2) n.toBigInteger() else impl(n)
 
-    fun impl(n: Int): BigInteger {
-        return cache.computeIfAbsent(n) { impl(it - 1) + impl(it - 2) }
-    }
-
-
-}
-
-
-data class UserData(
-    val id: Long,
-    val name: String,
-    val age: Int
-) : AbstractCoroutineContextElement(UserData) {
-    companion object Key : CoroutineContext.Key<UserData>
-
-    val userData = UserData(1, "name1", 10)
-    val scope = CoroutineScope(Job() + Dispatchers.Default + userData)
-
-
-}
--- 13_Coroutines/src/main/kotlin/Main.kt
+++ 13_Coroutines/src/main/kotlin/Main.kt
@@ -5,47 +5,30 @@ suspend fun main() {
     val start = System.currentTimeMillis()
     coroutineScope {
         val job = async(Dispatchers.IO) {
-            // delay(1000)
             val a = Fib.takeIter(100000)
-            // println("firs ${(System.currentTimeMillis() - start) / 1000F} $a ")
             return@async a
         }
-
         val job2 = async(Dispatchers.Default) {
-
-            val resss = withTimeout(50L) {
+            val resss = withTimeout(500L) {
                 val b = Fib.takeIter(100000)
                 return@withTimeout b
             }
-
-            // println("second ${(System.currentTimeMillis() - start) / 1000F} $b ")
             return@async resss
         }
         launch {
             while (true) {
                 delay(100)
                 print(".")
-
                 if (job.isCompleted) {
-                    // val result:BigInteger
-                    //val res = withTimeout(5) {
                     val result = job.await()
-
-
                     println()
                     println("первая  ${(System.currentTimeMillis() - start) / 1000F}  $result")
-
                     cancel()
                 }
 
                 if (job2.isCompleted) {
-                    // val result:BigInteger
-                    //val res = withTimeout(5) {
-
                     val result2 = job2.await()
-
                     println()
-
                     println("вторая  ${(System.currentTimeMillis() - start) / 1000F}  $result2")
                     cancel()
                 }
