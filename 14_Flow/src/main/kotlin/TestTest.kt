79220fc6acceb670dfb743e242a7ab3e76b67356
Andrey <phobos05@bk.ru>
2022-04-25 08:25:29 +0700

    first commit 25_04_22

--- 14_Flow/src/main/kotlin/TestTest.kt
+++ 14_Flow/src/main/kotlin/TestTest.kt
@@ -3,6 +3,11 @@ import kotlin.random.Random
 fun main() {
     val aa = LotoCard().createCard()
     aa.forEach { println("${it.listOfCellInRow}") }
+    println()
+
+    val bb = LotoCard().createCard()
+    bb.forEach { println("${it.listOfCellInRow}") }
+
 }
 
 class LotoCard {
@@ -15,94 +20,89 @@ class LotoCard {
             val row = Row()
             while (row.listOfCellInRow.size < 9) {
                 val cell = Cell()
-                val rnd1 = Random.nextInt(1, 10)
-                val rnd2 = Random.nextInt(11, 20)
-                val rnd3 = Random.nextInt(21, 30)
-                val rnd4 = Random.nextInt(31, 40)
-                val rnd5 = Random.nextInt(41, 50)
-                val rnd6 = Random.nextInt(51, 60)
-                val rnd7 = Random.nextInt(61, 70)
-                val rnd8 = Random.nextInt(71, 80)
-                val rnd9 = Random.nextInt(81, 90)
-
                 when (row.listOfCellInRow.size) {
-                    0 -> while (cell.value==null) {
-                        val rnd1 = Random.nextInt(1, 10)
-                        if (!tmpList.contains(rnd1)) {
-                            tmpList.add(rnd1)
-                            cell.value=rnd1
+                    0 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(1, 10)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    /*0 -> if (!tmpList.contains(rnd1)) {
-                        tmpList.add(rnd1)
-                        cell.value = rnd1
-                    }*/
-
-                    1 -> while (cell.value==null) {
-                        val rnd2 = Random.nextInt(11, 20)
-                        if (!tmpList.contains(rnd2)) {
-                            tmpList.add(rnd2)
-                            cell.value=rnd2
+                    1 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(11, 20)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    2 ->while (cell.value==null) {
-                        val rnd3 = Random.nextInt(21, 30)
-                        if (!tmpList.contains(rnd3)) {
-                            tmpList.add(rnd3)
-                            cell.value=rnd3
+                    2 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(21, 30)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    3 -> while (cell.value==null) {
-                        val rnd4 = Random.nextInt(31, 40)
-                        if (!tmpList.contains(rnd4)) {
-                            tmpList.add(rnd4)
-                            cell.value=rnd4
+                    3 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(31, 40)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    4 -> while (cell.value==null) {
-                        val rnd5 = Random.nextInt(41, 50)
-                        if (!tmpList.contains(rnd5)) {
-                            tmpList.add(rnd5)
-                            cell.value=rnd5
+                    4 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(41, 50)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    5 -> while (cell.value==null) {
-                        val rnd6 = Random.nextInt(51, 60)
-                        if (!tmpList.contains(rnd6)) {
-                            tmpList.add(rnd6)
-                            cell.value=rnd6
+                    5 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(51, 60)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    6 -> while (cell.value==null) {
-                        val rnd7 = Random.nextInt(61, 70)
-                        if (!tmpList.contains(rnd7)) {
-                            tmpList.add(rnd7)
-                            cell.value=rnd7
+                    6 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(61, 70)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    7 -> while (cell.value==null) {
-                        val rnd8 = Random.nextInt(71, 80)
-                        if (!tmpList.contains(rnd8)) {
-                            tmpList.add(rnd8)
-                            cell.value=rnd8
+                    7 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(71, 80)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
-                    8 -> while (cell.value==null) {
-                        val rnd9 = Random.nextInt(81, 90)
-                        if (!tmpList.contains(rnd9)) {
-                            tmpList.add(rnd9)
-                            cell.value=rnd9
+                    8 -> while (cell.value == null) {
+                        val rnd = Random.nextInt(81, 90)
+                        if (!tmpList.contains(rnd)) {
+                            tmpList.add(rnd)
+                            cell.value = rnd
                         }
                     }
                 }
-
-
                 row.listOfCellInRow.add(cell)
 
 
             }
             listOfLotoCard.add(row)
-            //listOfLotoCard.forEachIndexed { index, row -> row.cell.value}
+
+
+        }
+
+        for (i in 0 until listOfLotoCard.size) {
+            val ss = listOfLotoCard[i].listOfCellInRow
+            var count = 0
+            while (count < 4) {
+                val rnd = Random.nextInt(0, 8)
+                listOfLotoCard[i].listOfCellInRow[rnd].value = "  "
+                count = ss.count { s -> (s.value == "  ") }
+            }
+
         }
         return listOfLotoCard
     }
@@ -110,6 +110,7 @@ class LotoCard {
 
 class Row {
     var listOfCellInRow = mutableListOf<Cell>()
+
     val cell = Cell().createCell()
     fun createRow(): MutableList<Cell> {
         val tmpList = hashSetOf<Int>()
@@ -127,14 +128,17 @@ class Row {
 }
 
 class Cell {
-    var value: Int? = null
-    var state: Any? = null
+    var value: Any? = null
+    var isChecked = false
 
     fun createCell(): Cell {
         return Cell()
     }
 
     override fun toString(): String {
+        if (isChecked) {
+            value = "x"
+        }
         return "$value"
     }
 }
